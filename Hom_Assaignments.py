95408ce8d728da3e7d472cd446cd9ca5b2caa2c2
Muhammad Shahid <shahidjan0021@gmail.com>
2025-01-11 12:29:56 +0500

    Initial Commit

---
+++ Hom_Assaignments.py
@@ -0,0 +1,22 @@
+# privious class k end pr Sir Aneeq ne hmen 2 assaignment
+# diye jin me se 1st 2 ka table banana tha 
+# or 2nd Grade base Marksheet Bnani hai.
+
+
+# 1st assaignment
+
+# table of "2"
+# is table ko ham banay jarhy hen 
+
+
+# for a in range(1,11):
+#     print("2 x",a,"=",2*a)
+
+# is assaignment ko ham samjhny ki koshish krty hen
+# (for) python ka 1 function hai jis ki mdad se ham bar bar repeat hony
+# waly kamm ko python ka 1 function use kr k one line of code 
+# me baasani kr paty hen.
+# is ki assan exemple ye hai. jesa k ham kisi bhi code ko  
+# print krany k liye print statment use krty hen jesa k
+
+print("2 x 1 = 2")
